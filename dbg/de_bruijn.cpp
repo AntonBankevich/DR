291efb3d13581458b52ade73117f18e18b76f29a
Anton Bankevich <anton.bankevich@gmail.com>
2020-07-21 15:55:02 -0700

    DBG wip

--- dbg/de_bruijn.cpp
+++ dbg/de_bruijn.cpp
@@ -1,94 +1,41 @@
 //
 // Created by anton on 17.07.2020.
 //
-
-#pragma once
-
-//
-// Created by anton on 08.07.2020.
-//
+#define _GLIBCXX_PARALLEL
 #include <iostream>
 #include <sequences/contigs.hpp>
 #include <sequences/seqio.hpp>
 #include <common/dir_utils.hpp>
 #include <queue>
+#include <omp.h>
 #include "common/cl_parser.hpp"
-#include "aligner.hpp"
+#include "rolling_hash.hpp"
+#include<parallel/algorithm>
+#include <sstream>
 
-typedef unsigned __int128 htype;
 
-htype pow(htype base, size_t p) {
-    if (p == 0)
-        return 1;
-    htype tmp = pow(base, p / 2);
-    if (p %2 == 1)
-        return base * tmp * tmp;
-    else
-        return tmp * tmp
-}
+typedef unsigned __int128 htype128;
 
-const size_t k = 7000;
-const size_t hbase = 239;
-const htype kpow = pow(hbase, k - 1);
-w = 12000;
-
-class KWH {
+class Time {
+private:
+    timespec start{};
 public:
-    Sequence *seq;
-    size_t pos;
-    htype hash;
-    KWH(Sequence *_seq, size_t _pos): seq(_seq), pos(_pos) {
-        hash = 0;
-        for(size_t i = _pos; i < _pos + k; i++) {
-            hash = hash * hbase + seq->operator[](i);
-        }
+    Time() {
+        clock_gettime(CLOCK_MONOTONIC, &start);
     }
 
-    KWH(Sequence *_seq, size_t _pos, size_t _hash): seq(_seq), pos(_pos), hash(_hash) {
-    }
-
-    bool isMovable() {
-        return seq == nullptr;
-    }
-
-    KWH next() const {
-        htype other_hash = (hash - kpow * dignucl(seq->operator[](pos))) * hbase + seq->operator[](pos + k);
-        return KWH(seq, pos + 1, other_hash);
-    }
-
-    htype operator<<(char c) const {
-        return (hash - kpow * dignucl(seq->operator[](pos))) * hbase + c;
-    }
-};
-
-class MinQueue {
-    std::deque<KWH> q;
-public:
-    MinQueue() = default;
-
-    void push(const KWH &kwh) {
-        while(!q.empty() && q.back().hash >= kwh.hash) {
-            q.pop_back();
-        }
-        q.push_back(kwh);
-    }
-
-    void pop(size_t pos) {
-        if(!q.empty() && q.front().pos <= pos) {
-            q.pop_front();
-        }
-    }
-
-    bool empty() const {
-        return q.empty();
-    }
-
-    KWH get() const {
-        return q.front();
+    string get() {
+        timespec finish{};
+        clock_gettime(CLOCK_MONOTONIC, &finish);
+        size_t worktime = size_t(double(finish.tv_sec - start.tv_sec) + double(finish.tv_nsec - start.tv_nsec) / 1000000000.0);
+        std::stringstream ss;
+        ss << worktime / 60 / 60 << ":" << worktime / 60 % 60 << ":" << worktime % 60 << " ";
+        return ss.str();
     }
 };
 
 int main(int argc, char **argv) {
+    Time time;
     CLParser parser({"reads=", "output-dir=", "threads=8"}, {"o=output-dir", "t=threads"});
     parser.parseCL(argc, argv);
     if (!parser.check().empty()) {
@@ -98,18 +45,45 @@ int main(int argc, char **argv) {
     }
     std::cout << "Reading reads" <<std::endl;
     io::SeqReader reader(parser.getValue("reads"));
-    std::vector<htype> hashs;
+    std::vector<std::vector<htype128>> hashs;
+    size_t threads = std::stoi(parser.getValue("threads"));
+    hashs.resize(threads);
+    const size_t buffer_size = 1000000000;
+    const RollingHash<htype128> hasher(7000, 239);
+    omp_set_num_threads(threads);
+    std::cout << time.get() << "Extracting minimizers" << std::endl;
     while(not reader.eof()) {
-        Contig read = reader.read();
-
+        size_t tlen = 0;
+        std::cout << time.get() << "Starting new round" << std::endl;
+#pragma omp parallel default(none) shared(hasher, hashs, reader, tlen, buffer_size, std::cout, time)
+        {
+#pragma omp single
+            {
+                while (not reader.eof() and tlen < buffer_size) {
+                    const Contig read = reader.read();
+                    tlen += read.size();
+#pragma omp task default(none) shared(read, hasher, hashs, std::cout)
+                    {
+                        MinimizerCalculator<htype128> calc(read.seq, hasher, 4000);
+                        std::vector<htype128> k_mers(calc.minimizers());
+                        std::vector<htype128> & thread_hashs = hashs[omp_get_thread_num()];
+                        thread_hashs.insert(thread_hashs.end(), k_mers.begin(), k_mers.end());
+                    }
+                }
+                std::cout << time.get() << tlen  << " nucleotides read from disk. Processing in progress" << std::endl;
+            }
+        }
+        std::cout << time.get() << "Processing finished" << std::endl;
     }
-    std::experimental::filesystem::path dir(parser.getValue("output-dir"));
-    ensure_dir_existance(dir);
-    cout << std::experimental::filesystem::absolute(dir) << endl;
-    std::ofstream os;
-    os.open(dir / "log.info");
-    std::cout << "Printing results" <<std::endl;
-    os.close();
-    std::cout << "Finished" <<std::endl;
+    std::cout << time.get() << "Finished read processing" << std::endl;
+    std::vector<htype128> hash_list;
+    for (auto & hash : hashs) {
+        hash_list.insert(hash_list.end(), hash.begin(), hash.end());
+    }
+    std::cout << time.get() << "Finished merginf results: " << hash_list.size() << " hashs collected. Starting sorting." << std::endl;
+              //    TODO replace with parallel std::sort
+    __gnu_parallel::sort(hash_list.begin(), hash_list.end());
+    hash_list.erase(std::unique(hash_list.begin(), hash_list.end()), hash_list.end());
+    std::cout << time.get() << "Finished sorting. Total distinct minimizers: " << hash_list.size() << std::endl;
     return 0;
 }
