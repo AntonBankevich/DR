7cc730f14f84cda10d8d515db558105332fca05f
Anton bankevich <anton.bankevich@gmail.com>
2021-02-02 14:53:41 -0800

    Removed accidental code from dbg. Continued extraction implementation

--- src/projects/dbg/dbg.cpp
+++ src/projects/dbg/dbg.cpp
@@ -353,43 +353,6 @@ int main(int argc, char **argv) {
         }
     }
 
-    std::vector<std::string> extract_segments = parser.getListValue("extract-segment");
-    if(!path_segments.empty()) {
-        logger.info() << "Printing segments of paths" << std::endl;
-        logger.info() << "Aligning paths" << std::endl;
-        GraphAlignmentStorage<htype128> storage(dbg);
-        std::vector<std::tuple<std::string, size_t, size_t, std::string>> seg_recs;
-        for(const std::string& s : path_segments) {
-            std::vector<std::string> parsed = split(s, "[,]");
-            seg_recs.emplace_back(parsed[0], std::stoull(parsed[1]), std::stoull(parsed[2]), s);
-        }
-        std::vector<Contig> segs;
-        io::SeqReader reader(paths_lib);
-        for(StringContig scontig : reader) {
-            Contig contig = scontig.makeContig();
-            storage.fill(contig);
-            for(auto & seg_rec : seg_recs) {
-                if(std::get<0>(seg_rec) == contig.id) {
-                    segs.emplace_back(contig.seq.Subseq(std::get<1>(seg_rec), std::get<2>(seg_rec)), std::get<3>(seg_rec));
-                } else if (std::get<0>(seg_rec) == "-" + contig.id) {
-                    segs.emplace_back((!contig.seq).Subseq(std::get<1>(seg_rec), std::get<2>(seg_rec)), std::get<3>(seg_rec));
-                }
-            }
-        }
-        for(Contig &seg : segs) {
-            const std::experimental::filesystem::path seg_file = dir / ("seg_" + seg.id + ".dot");
-            logger.info() << "Printing segment " << seg.id << " to file dot file " << (seg_file) << std::endl;
-            std::ofstream coordinates_dot;
-            Component<htype128> comp = Component<htype128>::neighbourhood(dbg, seg, dbg.hasher().k + 100);
-            coordinates_dot.open(seg_file);
-            std::function<std::string(Edge<htype128> &)> labeler = [&storage](Edge<htype128> &edge) {
-                return storage(edge);
-            };
-            comp.printDot(coordinates_dot, labeler);
-            coordinates_dot.close();
-        }
-    }
-
     if(parser.getCheck("genome-path")) {
         logger.info() << "Printing additional figures with reference alignments" << std::endl;
         logger.info() << "Aligning genome" << std::endl;
