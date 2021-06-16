47edf95abf75cdbcc98147b71813cc8f27b6b983
Anton bankevich <anton.bankevich@gmail.com>
2021-06-16 20:47:46 +0300

    Script for rc removal

---
+++ src/projects/scripts/remove_rc.cpp
@@ -0,0 +1,15 @@
+#include <sequences/verify.hpp>
+#include <sequences/seqio.hpp>
+
+int main(int argc, char **argv) {
+    VERIFY(argc == 2);
+    StringContig::needs_compressing = false;
+    io::Library libReads = {std::experimental::filesystem::path(argv[1])};
+    io::SeqReader reader(libReads);
+    std::vector<std::pair<Sequence, Contig>> contigs;
+    for(StringContig s : reader) {
+        Contig c = s.makeContig();
+        if(c.seq <= !c.seq)
+            std::cout << c.id << "\n" << c.seq << "\n";
+    }
+}
