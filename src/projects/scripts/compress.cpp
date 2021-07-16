0bbadb779766952f3e9ef878684f47625906534b
Anton bankevich <anton.bankevich@gmail.com>
2021-07-16 16:04:05 +0300

    Compress script improved

--- src/projects/scripts/compress.cpp
+++ src/projects/scripts/compress.cpp
@@ -1,11 +1,16 @@
 #include <common/verify.hpp>
 #include <sequences/seqio.hpp>
+#include <common/cl_parser.hpp>
 
 int main(int argc, char **argv) {
-    VERIFY(argc == 2);
+    CLParser parser({"dimer-compress=1000000000,1000000000,1"},
+                    {"reads"}, {},"");
+    parser.parseCL(argc, argv);
     StringContig::homopolymer_compressing = true;
-    io::Library libReads = {std::experimental::filesystem::path(argv[1])};
-    io::SeqReader reader(libReads);
+    StringContig::SetDimerParameters(parser.getValue("dimer-compress"));
+
+    io::Library reads_lib = oneline::initialize<std::experimental::filesystem::path>(parser.getListValue("reads"));
+    io::SeqReader reader(reads_lib);
     for(StringContig s : reader) {
         Contig c = s.makeContig();
         std::cout << ">" << c.id << "\n" << c.seq << "\n";
