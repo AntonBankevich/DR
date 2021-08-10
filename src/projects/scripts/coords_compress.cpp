1df1109dc7787f972564f6c9d8182b4968f75559
Dmitry <dmitrij.antipov@gmail.com>
2021-08-10 15:53:31 +0300

    coords compress updated for dinucleotide

--- src/projects/scripts/coords_compress.cpp
+++ src/projects/scripts/coords_compress.cpp
@@ -13,6 +13,48 @@
 using namespace std;
 using logging::Logger;
 
+
+//compypaste!!!!
+size_t compressRead(const string& read, size_t compression_length, size_t pos) {
+//TODO:: is it fast?
+//        logger.trace() << read << endl;
+    string res = "";
+    size_t current_coord = 0;
+    for (size_t i = 0; i < read.length(); i++) {
+        if (i == pos)
+            return current_coord;
+        bool compressed = false;
+        if (i > 0 && read[i] == read[i - 1]) {
+            compressed = true;
+        } else if (current_coord > 2*compression_length) {
+            bool in_repeat = (read[i] ==res[current_coord - 2] || read[i] == res[current_coord - 1]);
+            if (in_repeat) {
+                for (size_t j = 1; j < compression_length; j++) {
+                    if (res[current_coord  - 2 * j] != res[current_coord -2]) {
+                        in_repeat = false;
+                        break;
+                    }
+                }
+            }
+            if (in_repeat) {
+                for (size_t j = 1; j < compression_length; j++) {
+                    if (res[current_coord -1 - 2 * j] != res[current_coord -1]) {
+                        in_repeat = false;
+                        break;
+                    }
+                }
+            }
+            compressed |= in_repeat;
+        }
+        if (!compressed) {
+            res = std::move(res) + read[i];
+            current_coord ++;
+        }
+    }
+//        logger.trace() << res.str() << endl;
+    return -1;
+}
+
 int main(int argc, char **argv) {
     CLParser parser({"ref=", "position=", "name="}, {},
                     {});
@@ -37,11 +79,9 @@ int main(int argc, char **argv) {
         cout << "Wrong name";
         return 0;
     }
-    int comp_pos = 0;
-    for (int i = 0; i < pos; i++) {
-        if (assembly[pos_name][i] != assembly[pos_name][i+1])
-            comp_pos ++;
-    }
+
+    int comp_pos = compressRead(assembly[pos_name].str(), 16, pos);
+
     cout << name << " " <<comp_pos << " " << pos << " " << assembly[0][pos];
     return 0;
 }
