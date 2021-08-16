f62e892190c85c5528bfbb5d677ec2b9ee63d17a
Dmitry Antipov <dmitrij.antipov@gmail.com>
2021-08-16 15:12:04 +0300

    fix for extraction script

--- src/projects/scripts/homopolish_supplementary/extract_reads.py
+++ src/projects/scripts/homopolish_supplementary/extract_reads.py
@@ -9,7 +9,7 @@ to_use = False
 id = ""
 #grep -P 'ccs 21|ccs -21'
 for line in open (sys.argv[2]):
-    if line[0] == ">" or line[0] == "@":
+    if (line[0] == ">" or line[0] == "@") and line[1] == "S" and line[2] == "R" and line[3] == "X":
         id = line.strip().split()[0][1:]
         if id in used_names:
             print (line[0] + id)        
